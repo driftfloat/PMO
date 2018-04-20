85c12bd54df65b79d80df71c128e21d76d60486a
dapengchina <aoqishequ@163.com>
2018-04-20 10:55:43 +0800

    Change-Id: Icbd424e7719e0e7cd7bab5b1bb7c533391250dc4

--- src/main/java/com/pmo/dashboard/util/Constants.java
+++ src/main/java/com/pmo/dashboard/util/Constants.java
@@ -9,4 +9,8 @@ public class Constants
     public final static String RESUME_PATH = "D:/Resume/";
     public final static int PAGE_DATA_COUNT = 10;
    /* public final static int USER_PASSWORD = 123;*/
+    public static final String IS_MAIN_ABILITY ="0";//是主能力
+    public static final String NOT_MAIN_ABILITY = "1";//不是主能力
+    public static final String IS_OFFICIAL_ACCREDITATION ="0";//是官方认证
+    public static final String NOT_OFFICIAL_ACCREDITATION ="1";//不是官方认证
 }
