c1cc07cd8f82fe60a57bb41c17d7bcddd9ecfe98
driftfloat <fallwater@126.com>
2018-04-30 14:50:14 +0800

    export excel

--- src/test/java/test/MvcTest.java
+++ src/test/java/test/MvcTest.java
@@ -12,11 +12,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 		"classpath:/conf/spring-mybatis.xml"
 //		,"file:src/main/webapp/WEB-INF/dispatcherServlet-servlet.xml" 
-		,"classpath:/conf/spring-mvc.xml"
+//		,"classpath:/conf/spring-mvc.xml"
 		})
 @WebAppConfiguration
 public class MvcTest {
@@ -31,6 +32,9 @@ public class MvcTest {
 	
 	@Test
 	public void testPage() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/offlineOper/query").param("pageNumber", "1")).andReturn();
+		// http://localhost:8080/Pmo/service/offlineOper/query?pageSize=10&pageNumber=1&_=1525055119537
+		mockMvc.perform(MockMvcRequestBuilders.get("/service/offlineOper/query").param("pageNumber", "1")).andReturn();
+//		mockMvc.perform(MockMvcRequestBuilders.get("/service/offlineOper/query").param("pageNumber", "1"))
+//			.andExpect(view())
 	}
 }
