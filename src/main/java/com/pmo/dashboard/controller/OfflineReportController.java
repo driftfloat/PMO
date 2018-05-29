2f00dc0cc8f7c9cfa211fbf7e43c6aa4c84f587c
dapengchina <aoqishequ@163.com>
2018-05-29 10:28:46 +0800

    Change-Id: I27ea168861c0f28e2be8b9060abd67c2996db65c

--- src/main/java/com/pmo/dashboard/controller/OfflineReportController.java
+++ src/main/java/com/pmo/dashboard/controller/OfflineReportController.java
@@ -1,19 +1,14 @@
 package com.pmo.dashboard.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.pmo.dashboard.entity.CSDept;
-import com.pmo.dashboard.entity.User;
-
 @Controller
 @RequestMapping(value="/offline/report")
-public class TestController {
+public class OfflineReportController {
 	
 	@RequestMapping("/listPage")
 	public String listPage(HttpServletRequest request,Model model){
